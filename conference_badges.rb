78a2ee159f619ccbd0829a7027e569afe23c2905
Breanne <BreMarotta@gmail.com>
2022-03-09 10:49:32 -0500

    commit tree 08a4edda4770202f18c9026304002e2e7186bb4e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |name| "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(array)
+    array.map.with_index(1) do |name, room_num| "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).map do |badge|
+        puts badge
+    end
+    assign_rooms(array).each do |room|
+        puts room 
+    end
+end
